b95c24f653a05a65f99610288f35b90bab49f32c
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-02 02:06:11 +0900

    ADD: Board

--- board/src/app.module.ts
+++ board/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [UsersModule, BoardModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })

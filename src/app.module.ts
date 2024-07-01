0911e048a725959abdd87f0ee3ba7fbfd7f745d1
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-02 02:06:52 +0900

    ADD: comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BoardModule } from './board/board.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [UsersModule, BoardModule],
+  imports: [UsersModule, BoardModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })

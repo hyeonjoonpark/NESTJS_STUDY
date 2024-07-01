2579aa3189cf881d4b56f8fa09b628421ded7c1a
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-02 02:04:25 +0900

    ADD: user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn({ name: 'id', length: 255 })
+  id: string;
+
+  @Column({
+    name: 'password',
+    length: 255,
+    nullable: false, // not null
+  })
+  password: string; // 비밀번호
+
+  @Column({
+    name: 'email',
+    length: 255,
+    unique: true, // unique
+  })
+  email: string; // 이메일
+
+  @Column({ name: 'created_at' })
+  createdAt: Date; // 가입 일자
+}
